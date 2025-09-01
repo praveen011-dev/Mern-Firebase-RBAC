76e12d636413d46fbfa09fcae45eb26700e2f93e
praveen <palpraveen3221@gmail.com>
2025-09-01 18:23:47 +0530

    update env with correct credetials

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { getProfile, login, signup } from "../controller/user.controller.js";
+import { getProfile } from "../controller/user.controller.js";
 import verifyAuth from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.route("/signup").post(signup);
-router.route("/login").post(login);
 router.route("/profile").get(verifyAuth, getProfile);
 
 export default router;
